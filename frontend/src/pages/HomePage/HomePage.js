91ee3099310272374f3cf6e7f434148a1c5796b5
Tedwards <108353463+Tedwards22890@users.noreply.github.com>
2022-11-01 17:10:50 -0400

    Added search bar to homepage with button

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
+import "./HomePage.css";
 
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -8,7 +9,6 @@ import { useNavigate } from "react-router-dom";
 const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
-  //TODO: Add an AddCars Page to add a car for a logged in user's garage
   const [user, token] = useAuth();
   const navigate = useNavigate();
   const [videos, setVideos] = useState([])
@@ -21,7 +21,7 @@ const HomePage = () => {
       }
     }, [token]);
 
-    async function fetchSearchResults(query="Halo") {
+    async function fetchSearchResults(query="Halo", KEY="AIzaSyAYsQ8hijKDgFdCpsasYOnOqRv-FoL7SGQ") {
       try {
         let response = await axios.get(`http://googleapis.com/youtube/v3/search?q=${query}&key=${KEY}&type=video&maxResults=5&part=snippet`);
         setVideos(response.data.items);
@@ -46,11 +46,20 @@ const HomePage = () => {
   
 
   return (
+    <div className="container">
+      <div className="form-group">
+        <form onSubmit={handleSubmit} className="form-grid">
+            <input type="text" placeholder="Name" value={query} onChange={(event) => setQuery(event.target.value)}/>
+            <button type="submit">Search</button>
+        </form>
+      </div>
+      {fetchSearchResults}
+    </div>
     
 
 
   );
-}
+};
 
 
 export default HomePage;
