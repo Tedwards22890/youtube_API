5530edb4e10fab73ce5e86f32ba6a0acab02e854
Tedwards <108353463+Tedwards22890@users.noreply.github.com>
2022-11-01 14:44:15 -0400

    Added basic logic for homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -36,3 +36,4 @@ function App() {
 }
 
 export default App;
+//API Key: AIzaSyAYsQ8hijKDgFdCpsasYOnOqRv-FoL7SGQ
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -3,40 +3,54 @@ import { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   //TODO: Add an AddCars Page to add a car for a logged in user's garage
   const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+  const navigate = useNavigate();
+  const [videos, setVideos] = useState([])
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
-    const fetchCars = async () => {
+    let mounted=true;
+    if (mounted) {
+      fetchSearchResults();
+      }
+    }, [token]);
+
+    async function fetchSearchResults(query="Halo") {
       try {
-        let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
-          headers: {
-            Authorization: "Bearer " + token,
-          },
-        });
-        setCars(response.data);
-      } catch (error) {
-        console.log(error.response.data);
+        let response = await axios.get(`http://googleapis.com/youtube/v3/search?q=${query}&key=${KEY}&type=video&maxResults=5&part=snippet`);
+        setVideos(response.data.items);
+        console.log(response.data.items)
+      } catch (error){
+          console.log(error.message);
       }
     };
-    fetchCars();
-  }, [token]);
+
+    const handleClick = (video) => {
+      navigate(`ViewVideo/${video.videoId}`, {
+        state: {
+          title: video.title,
+          description: video.description
+        }
+      })
+    };
+    function handleSubmit(event) {
+      event.preventDefault();
+      fetchSearchResults(query)
+    }
+  
+
   return (
-    <div className="container">
-      <h1>Home Page for {user.username}!</h1>
-      {cars &&
-        cars.map((car) => (
-          <p key={car.id}>
-            {car.year} {car.model} {car.make}
-          </p>
-        ))}
-    </div>
+    
+
+
   );
-};
+}
+
 
 export default HomePage;
